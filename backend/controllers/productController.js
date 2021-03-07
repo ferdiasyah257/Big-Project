0d061b6e17bf8937d55a3a8f9e16e4443a093c04
Muhammad Ferdiansyah <muhammad.ferdiansyah25@gmail.com>
2021-03-08 05:29:19 +0700

    third

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -21,8 +21,8 @@ exports.newProduct = catchAsyncErrors (async (req, res, next) => {
 //get all products => /api/v1/products?keyword=apple
 exports.getProducts = catchAsyncErrors (async (req, res, next) => {
 
-    const resPerPage = 4;
-    const productCount = await Product.countDocuments()
+    const resPerPage = 8;
+    const productsCount = await Product.countDocuments()
     
     const apiFeatures = new APIFeatures(Product.find(), req.query)
                         .search()
@@ -31,12 +31,13 @@ exports.getProducts = catchAsyncErrors (async (req, res, next) => {
 
     const products = await apiFeatures.query;
 
-    res.status(200).json({
-        success: true,
-        count: products.length,
-        productCount,
-        products
-    })
+        res.status(200).json({
+            success: true,
+            productsCount,
+            products
+        })
+
+
 })
 
 // Get single product details => /api/v1/product/:id
